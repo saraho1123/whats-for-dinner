e623df78ec1b34f2ec41a6469aaf55b7fc60e362
Sarah Osgood <sarahjs117@gmail.com>
2020-09-04 15:39:59 -0500

    Competed functionality of  lets cook related buttons without CSS formating of displayed innerText

--- main.js
+++ main.js
@@ -46,3 +46,53 @@ var desserts = [
   'Croissants',
   'Eclairs',
 ]
+var suggested;
+
+var letsCookButton = document.querySelector(".lets-cook-button")
+var side = document.querySelector(".side");
+var mainDish = document.querySelector(".main-dish");
+var dessert = document.querySelector(".dessert");
+var entireMeal = document.querySelector(".entire-meal");
+var foodSuggestion = document.querySelector(".food-suggestion");
+var cookpot = document.querySelector(".cookpot");
+var suggestion = document.querySelector(".suggestion");
+
+letsCookButton.addEventListener('click', displaySuggestion);
+side.addEventListener("click", sideSuggestion);
+mainDish.addEventListener("click", mainDishSuggestion);
+dessert.addEventListener("click", dessertSuggestion);
+// entireMeal.addEventListener("click", entireMealSuggestion);
+
+function getRandomFoods(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function displaySuggestion() {
+  foodSuggestion.innerText = `${suggested}!`;
+  toggleCookpot();
+}
+// refactor this function!
+
+function sideSuggestion() {
+  suggested = sides[getRandomFoods(sides)];
+  }
+
+function mainDishSuggestion() {
+  suggested = sides[getRandomFoods(sides)];
+  }
+
+function dessertSuggestion() {
+  suggested = sides[getRandomFoods(sides)];
+  }
+
+// function entireMealSuggestion() {
+//   for (var i = 0; i < sides.length; i++) {
+//     suggested = sides[i];
+//   }
+//   return suggested;
+// }
+
+function toggleCookpot() {
+  suggestion.classList.remove("hidden");
+  cookpot.classList.add("hidden");
+}
