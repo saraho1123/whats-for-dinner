2f04825a0b0569d7539d2683c1dd1dada89d6ba3
Sarah Osgood <sarahjs117@gmail.com>
2020-09-05 13:31:01 -0500

    Complete functionality of entire meal button option

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ var side = document.querySelector(".side");
 var mainDish = document.querySelector(".main-dish");
 var dessert = document.querySelector(".dessert");
 var entireMeal = document.querySelector(".entire-meal");
-var foodSuggestion = document.querySelector(".food-suggestion");
 var cookpot = document.querySelector(".cookpot");
+var foodSuggestion = document.querySelector(".food-suggestion");
+// var sideForMeal = document.querySelector(".side-for-meal");
+// var mainForMeal = document.querySelector(".main-for-meal");
+// var dessertForMeal = document.querySelector(".dessert-for-meal");
 var suggestion = document.querySelector(".suggestion");
 
 letsCookButton.addEventListener('click', displaySuggestion);
@@ -37,13 +40,20 @@ function dessertSuggestion() {
   suggested = desserts[getRandomFoods(desserts)];
   }
 
+function displaySuggestion() {
+  foodSuggestion.innerText = `${suggested}!`;
+  console.log(suggested);
+  toggleCookpot();
+}
+
 function entireMealSuggestion() {
-  var entireMeal = {
-    side: sides[getRandomFoods(sides)],
-    main: mains[getRandomFoods(mains)],
-    dessert: desserts[getRandomFoods(desserts)],
-  }
-  suggested = enire
+  var entireMeal = new Meal(
+    sides[getRandomFoods(sides)],
+    mains[getRandomFoods(mains)],
+    desserts[getRandomFoods(desserts)],
+  )
+  suggested = `${entireMeal.side}\n${entireMeal.main}\n${entireMeal.dessert}`;
+  return suggested;
 }
 
 function toggleCookpot() {
