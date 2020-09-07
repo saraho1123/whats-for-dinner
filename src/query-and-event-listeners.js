22728b6989246f335381c007e341acccd94897f1
Sarah Osgood <sarahjs117@gmail.com>
2020-09-06 22:05:56 -0500

    Move query selectors and event listeners to seperate js file

---
+++ src/query-and-event-listeners.js
@@ -0,0 +1,29 @@
+var letsCookButton = document.querySelector(".lets-cook-button")
+var side = document.querySelector(".side");
+var mainDish = document.querySelector(".main-dish");
+var dessert = document.querySelector(".dessert");
+var entireMeal = document.querySelector(".entire-meal");
+var cookpot = document.querySelector(".cookpot");
+var suggestion = document.querySelector(".suggestion");
+var clearButton = document.querySelector(".clear-button");
+var addRecipeButton = document.querySelector(".add-recipe-button");
+var recipeTypeInput = document.querySelector(".recipe-type");
+var recipeNameInput = document.querySelector(".recipe-name");
+var addNewButton = document.querySelector(".add-new");
+var userMealButton = document.querySelector(".user-meal");
+var htmlAlert = document.querySelector(".alert");
+
+// consider querySelectorAll for the radio buttons!
+// will need quite a bit of refactoring!
+// NOTE: I tried this and couldn't get it to work!!
+
+// I think want to put all my query selectors and event listeners in
+// a seperate .js file before I turn this in.
+
+letsCookButton.addEventListener("click", displaySuggestion);
+clearButton.addEventListener("click", clearSuggestions);
+addRecipeButton.addEventListener("click", showForm);
+addNewButton.addEventListener("click", displayNewUserIdea);
+userMealButton.addEventListener("click", displayUserWholeMeal);
+userMealButton.addEventListener("mouseover", toggleAlert);
+userMealButton.addEventListener("mouseleave", toggleAlert);
